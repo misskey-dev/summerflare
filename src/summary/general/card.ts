151d0b8d111921e3571c012a36e77aff870e025e
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-10-22 18:52:58 +0900

    fix: support `meta[name="twitter:card"]`

--- src/summary/general/card.ts
+++ src/summary/general/card.ts
@@ -3,10 +3,18 @@ import type { PrioritizedReference } from "../common";
 
 export default function getCard(url: URL, html: HTMLRewriter) {
   const result: PrioritizedReference<string | null> = {
-    bits: 1, // 0-1
+    bits: 2, // 0-3
     priority: 0,
     content: null,
   };
+  html.on('meta[name="twitter:card"]', {
+    element(element) {
+      const content = element.getAttribute("content");
+      if (content) {
+        assign(result, 2, content);
+      }
+    },
+  });
   html.on('meta[property="twitter:card"]', {
     element(element) {
       const content = element.getAttribute("content");
